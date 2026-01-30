34a5550ce15d0096fc53de497cb17c8f013f1c5b
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-01-30 21:58:16 +0700

    p-cv

--- p-cv/.vercel/output/functions/index.func/index.mjs
+++ p-cv/.vercel/output/functions/index.func/index.mjs
@@ -1,1 +1,19 @@
 // @ts-check
+
+export function GET() {
+  return fetch("https://github.com/haikalrowi/wsl/raw/HEAD/p-cv/README.md")
+    .then((r) => {
+      return r.text();
+    })
+    .then((text) => {
+      return fetch("https://api.github.com/markdown", {
+        method: "POST",
+        headers: { Accept: "text/html", "Content-Type": "application/json" },
+        body: JSON.stringify({ text: text }),
+      });
+    })
+    .then((r) => r.text())
+    .then((text) => {
+      return new Response(text, { headers: { "Content-Type": "text/html" } });
+    });
+}
