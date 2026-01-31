4d799eb445f1ea43b55717e438d38506a6bb9801
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-01-31 19:24:56 +0700

    script

--- script/github/get-commits-by-user.js
+++ script/github/get-commits-by-user.js
@@ -1,28 +1,29 @@
 // @ts-check
 
-"use strict";
-
 import fs from "node:fs";
 import path from "node:path";
 
 const githubSlashUser = new URL(`https://github.com/${process.argv[2]}`);
-const fromYear = +process.argv[3] || new Date().getFullYear();
-const toYear = +process.argv[4] || 2000;
+const startYear = +process.argv[3] || new Date().getFullYear();
+const endYear = +process.argv[4] || 2000;
 const output = path.resolve(process.cwd(), "output.json");
 
 (async () => {
   const years = [];
-  for (const iYear in Array.from({ length: fromYear - toYear })) {
-    for (const iMonth in Array.from({ length: 12 })) {
-      years.push(new Date(fromYear - +iYear, 11 - +iMonth));
+  for (const year in Array.from({ length: startYear - endYear })) {
+    for (const month in Array.from({ length: 12 })) {
+      years.push(new Date(startYear - +year, 11 - +month));
     }
   }
   const responses = [];
-  for (const item of years.flat(1)) {
+  for (const item of years) {
     const year = `${item.getFullYear()}`.padStart(4, "0");
     const month = `${item.getMonth() + 1}`.padStart(2, "0");
-    const url = `${githubSlashUser}?tab=overview&from=${year}-${month}-31&to=${year}-${month}-01`;
-    console.log(url);
+    const url = new URL(
+      `?tab=overview&from=${year}-${month}-31&to=${year}-${month}-01`,
+      githubSlashUser,
+    );
+    console.log(`${url}`);
     const fetched = fetch(url, {
       headers: { "x-requested-with": "XMLHttpRequest" },
     });
