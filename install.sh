ad03d61960f7421be73018f73af2c823c759966e
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-03-18 22:02:25 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,12 @@
 # curl -sL https://github.com/haikalrowi/wsl/raw/HEAD/install.sh | bash -i
 
-sudo -v &&
-(
-while true
-do
 sudo -v
-sleep 60
-done
-)
-
-&
 
 # pnpm and node.js
 
 curl -fsSL https://get.pnpm.io/install.sh | sh - &&
 . ~/.bashrc &&
 pnpm env use --global lts
-
-&&
 
 # ghcr.io/devcontainers/features/common-utils
 
@@ -30,13 +19,9 @@ curl -LO https://github.com/devcontainers/features/raw/HEAD/src/common-utils/mai
 sudo INSTALLZSH="false" CONFIGUREZSHASDEFAULTSHELL="false" INSTALLOHMYZSH="false" INSTALLOHMYZSHCONFIG="false" UPGRADEPACKAGES="false" bash install.sh
 )
 
-&&
-
 # bun
 
 curl -fsSL https://bun.com/install | bash
-
-&&
 
 # cloudflared
 
@@ -45,15 +30,11 @@ curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/s
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main" | sudo tee /etc/apt/sources.list.d/cloudflared.list &&
 sudo apt-get update && sudo apt-get install cloudflared
 
-&&
-
 # uv
 
 curl -LsSf https://astral.sh/uv/install.sh | sh &&
 . ~/.bashrc &&
 uv python install
-
-&&
 
 # poetry
 
