b037981d32259db4a705caa66d67f46f601294cc
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-04-04 16:36:56 +0700

    script

--- script/github/get-commits-by-user.js
+++ script/github/get-commits-by-user.js
@@ -3,20 +3,28 @@
 import fs from "node:fs";
 
 (async function (
-  githubUrl = new URL(`/${process.argv[2]}`, "https://github.com/"),
-  yearFrom = +process.argv[3] || new Date().getFullYear(),
-  yearTo = +process.argv[4] || 2000,
+  githubUrl = new URL(`/${process.argv[2]}`, "https://github.com/").href,
+  fromYear = new Date().getFullYear(),
+  toYear = 2000,
   output = `${new URL(import.meta.url).pathname}.txt`,
+  fetchConcurrency = 100,
+  fetchDelay = 120e3,
 ) {
   const years = [];
-  for (const year in Array.from({ length: 1 + yearFrom - yearTo })) {
+  for (const year in Array.from({ length: 1 + fromYear - toYear })) {
     for (const month in Array.from({ length: 12 })) {
-      years.push(new Date(yearFrom - +year, 11 - +month));
+      years.push(new Date(fromYear - +year, 11 - +month));
     }
   }
   const links = new Set();
   const linkPromises = [];
   for (const item of years) {
+    if (linkPromises.length && linkPromises.length % fetchConcurrency === 0) {
+      console.log(
+        `${Math.floor(linkPromises.length / fetchConcurrency)}/${Math.floor(years.length / fetchConcurrency)}`,
+      );
+      await new Promise((resolve) => global.setTimeout(resolve, fetchDelay));
+    }
     const linkPromise = (async () => {
       const year = Intl.DateTimeFormat("en", { year: "numeric" }).format(item);
       const month = Intl.DateTimeFormat("en", { month: "2-digit" }).format(
@@ -25,14 +33,17 @@ import fs from "node:fs";
       const dateTo = Intl.DateTimeFormat("en", { day: "2-digit" }).format(
         new Date(item.getFullYear(), item.getMonth() + 1, 0),
       );
-      githubUrl.search = `?tab=overview&from=${year}-${month}-01&to=${year}-${month}-${dateTo}`;
-      console.log(`${githubUrl}`);
-      const response = await fetch(githubUrl, {
+      const url = new URL(
+        `?tab=overview&from=${year}-${month}-01&to=${year}-${month}-${dateTo}`,
+        githubUrl,
+      );
+      const response = await fetch(url, {
         headers: { "x-requested-with": "XMLHttpRequest" },
       });
+      console.log(url.href, response.status);
       const text = await response.text();
       for (const item of text.matchAll(/href="(.*?\/commits\?author=.*?)"/g)) {
-        links.add(`${new URL(item[1], githubUrl)}`);
+        links.add(new URL(item[1], url).href);
       }
     })();
     linkPromises.push(linkPromise);
