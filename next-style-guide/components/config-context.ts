9bb350a2aaff623abd062e6582c3fa61623b4486
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-06-23 08:33:02 +0700

    next-style-guide

---
+++ next-style-guide/components/config-context.ts
@@ -0,0 +1,18 @@
+// "use client";
+
+// import { GetStory } from "@/utils/storyblok";
+// import { createContext, useContext } from "react";
+
+// export const ConfigContext = createContext<null | GetStory<"page_config">>(
+//   null,
+// );
+
+// export function useConfigContext() {
+//   const config = useContext(ConfigContext);
+
+//   if (!config) {
+//     throw new Error(useConfigContext.name);
+//   }
+
+//   return config;
+// }
