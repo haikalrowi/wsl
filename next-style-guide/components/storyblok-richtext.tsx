3f56ec752d8a0e9ea355523637180eef268a5746
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-07-04 07:45:35 +0700

    next-style-guide

--- next-style-guide/components/storyblok-richtext.tsx
+++ next-style-guide/components/storyblok-richtext.tsx
@@ -1,31 +1,31 @@
-import { cn } from "@/lib/utils";
-import { StoryblokRichtext } from "@/utils/storyblok";
-import { StoryblokRichText as StoryblokRichText_ } from "@storyblok/react";
+// import { cn } from "@/lib/utils";
+// import { StoryblokRichtext } from "@/utils/storyblok";
+// import { StoryblokRichText as StoryblokRichText_ } from "@storyblok/react";
 
-type StoryblokRichTextProps = React.ComponentProps<typeof StoryblokRichText_>;
+// type StoryblokRichTextProps = React.ComponentProps<typeof StoryblokRichText_>;
 
-export function StoryblokRichText({
-  StoryblokRichTextProps: { doc, ...StoryblokRichTextProps },
-  typographyClassName = true,
-  className,
-  ...props
-}: {
-  StoryblokRichTextProps: {
-    doc?: StoryblokRichtext;
-  } & Omit<StoryblokRichTextProps, "doc">;
-  typographyClassName?: boolean;
-} & React.ComponentProps<"div">) {
-  return (
-    <div
-      className={cn(typographyClassName && "typography", className)}
-      {...props}
-    >
-      {doc && (
-        <StoryblokRichText_
-          doc={doc as StoryblokRichTextProps["doc"]}
-          {...StoryblokRichTextProps}
-        ></StoryblokRichText_>
-      )}
-    </div>
-  );
-}
+// export function StoryblokRichText({
+//   StoryblokRichTextProps: { doc, ...StoryblokRichTextProps },
+//   typographyClassName = true,
+//   className,
+//   ...props
+// }: {
+//   StoryblokRichTextProps: {
+//     doc?: StoryblokRichtext;
+//   } & Omit<StoryblokRichTextProps, "doc">;
+//   typographyClassName?: boolean;
+// } & React.ComponentProps<"div">) {
+//   return (
+//     <div
+//       className={cn(typographyClassName && "typography", className)}
+//       {...props}
+//     >
+//       {doc && (
+//         <StoryblokRichText_
+//           doc={doc as StoryblokRichTextProps["doc"]}
+//           {...StoryblokRichTextProps}
+//         ></StoryblokRichText_>
+//       )}
+//     </div>
+//   );
+// }
