98894115e4288a1536f7719607c4e589ab5bee1f
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-07-15 20:29:40 +0700

    registry

---
+++ registry/src/next-storyblok/components-storyblok-context.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { GetStory } from "@/utils/storyblok";
+import { createContext, useContext } from "react";
+
+export const ConfigContext = createContext<null | GetStory<"page_config">>(
+  null,
+);
+export const PageContext = createContext<null | GetStory>(null);
+
+export function useConfigContext() {
+  const config = useContext(ConfigContext);
+
+  if (!config) {
+    throw new Error(ConfigContext.name);
+  }
+
+  return config;
+}
+
+export function usePageContext() {
+  const page = useContext(PageContext);
+
+  if (!page) {
+    throw new Error(PageContext.name);
+  }
+
+  return page;
+}
