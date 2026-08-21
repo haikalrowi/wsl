1014479d636672891ff58d11887ae926b9ae030e
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2026-08-21 20:56:26 +0700

    next-style-guide

--- next-style-guide/components/csr.tsx
+++ next-style-guide/components/csr.tsx
@@ -2,8 +2,8 @@
 
 import dynamic from "next/dynamic";
 
-const Csr_ = ({ children }: React.PropsWithChildren) => {
+function Csr_({ children }: React.PropsWithChildren) {
   return children;
-};
+}
 
 export const Csr = dynamic(async () => Csr_, { ssr: false });
